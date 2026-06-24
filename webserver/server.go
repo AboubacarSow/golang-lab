e71af061e3cb66ed22f74ab9eeae3bbf8211afba
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2026-06-25 01:41:28 +0300

    feat(webserver): add static html template for new project

--- webserver/server.go
+++ webserver/server.go
@@ -10,7 +10,8 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	port := ":8080"
-	fmt.Println("Server starts running on port" + port)
+	fmt.Print("Server starts running on port" + port)
+	fmt.Println(". Follow link: http://localhost" + port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal(err)
 	}
